6ac0fceb379126261d8e31ad3f7f8279e8f5d9ee
RustamA <rustam.alashrafov@gmail.com>
2019-03-03 14:56:10 +0100

    Added Double Sided shader.  Added pDolalr library.
    Added Double Sided shader.
    Added pDolalr library.

---
+++ Assets/GestureRecogniser.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PDollarGestureRecognizer;
+using System.IO;
+
+namespace AstroNet
+{
+    public class GestureRecogniser : MonoBehaviour
+    {
+
+        [SerializeField] public Transform gestureOnScreenPrefab;
+
+        private List<Gesture> trainingSet = new List<Gesture>();
+
+        private List<Point> points = new List<Point>();
+        private int strokeId = -1;
+
+        private Vector3 virtualKeyPosition = Vector2.zero;
+        private Rect drawArea;
+
+        private RuntimePlatform platform;
+        private int vertexCount = 0;
+
+        private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
+        private LineRenderer currentGestureLineRenderer;
+
+        private bool recognized;
+        // Start is called before the first frame update
+        void Start()
+        {
+            platform = Application.platform;
+            drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
+
+            //Load pre-made gestures
+            TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
+            foreach (TextAsset gestureXml in gesturesXml)
+                trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
+
+            //Load user custom gestures
+            string[] filePaths = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+            foreach (string filePath in filePaths)
+                trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+            {
+                if (Input.touchCount > 0)
+                {
+                    virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+                }
+            }
+
+            if (drawArea.Contains(virtualKeyPosition))
+            {
+
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    if (recognized)
+                    {
+
+                        recognized = false;
+                        strokeId = -1;
+
+                        points.Clear();
+
+                        foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+                        {
+
+                            lineRenderer.SetVertexCount(0);
+                            Destroy(lineRenderer.gameObject);
+                        }
+
+                        gestureLinesRenderer.Clear();
+                    }
+
+                    ++strokeId;
+
+                    Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+                    currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+
+                    gestureLinesRenderer.Add(currentGestureLineRenderer);
+
+                    vertexCount = 0;
+                }
+
+                if (Input.GetMouseButton(0))
+                {
+                    points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+
+                    currentGestureLineRenderer.SetVertexCount(++vertexCount);
+                    currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+                }
+            }
+        }
+    }
+}
