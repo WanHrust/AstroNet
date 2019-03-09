219ad0d32e5b27e41ec623cc368d0be0654dab0c
RustamA <rustam.alashrafov@gmail.com>
2019-03-09 17:25:14 +0100

    Finished gesture recongnition on pointer release.
    Tested on android build.

--- Assets/Scripts/GestureRecogniser.cs
+++ Assets/Scripts/GestureRecogniser.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using PDollarGestureRecognizer;
 using System.IO;
 using AstroNet.GameElements;
+using UnityEngine.EventSystems;
 namespace AstroNet
 {
-    public class GestureRecogniser : MonoBehaviour
+    public class GestureRecogniser : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
 
         [SerializeField] private Transform gestureOnScreenPrefab;
@@ -28,102 +29,105 @@ namespace AstroNet
         private LineRenderer currentGestureLineRenderer;
 
         private bool recognized;
+
+        private bool pointerDown;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            pointerDown = true;
+            if (recognized)
+            {
+
+                recognized = false;
+                strokeId = -1;
+
+                points.Clear();
+
+                foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+                {
+
+                    lineRenderer.SetVertexCount(0);
+                    Destroy(lineRenderer.gameObject);
+                }
+
+                gestureLinesRenderer.Clear();
+            }
+
+            ++strokeId;
+
+            Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+            currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+
+            gestureLinesRenderer.Add(currentGestureLineRenderer);
+
+            vertexCount = 0;
+
+            Debug.Log("OnPointerDoown");
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            pointerDown = false;
+            RecogniseDrawing();
+            Debug.Log("OnPointerUp");
+        }
+
         // Start is called before the first frame update
         void Start()
         {
             platform = Application.platform;
             drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
 
-            //Load pre-made gestures
-            TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
-            foreach (TextAsset gestureXml in gesturesXml)
-                trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
-
-            //Load user custom gestures
-            string[] filePaths = Directory.GetFiles(Application.persistentDataPath, "*.xml");
-            foreach (string filePath in filePaths)
-                trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
+            var files = Resources.LoadAll<TextAsset>("Gestures/") as TextAsset[];
+            foreach (TextAsset file in files)
+                trainingSet.Add(GestureIO.ReadGestureFromXML(file.text));
         }
 
+        private void RecogniseDrawing()
+        {
+            recognized = true;
+
+            Gesture candidate = new Gesture(points.ToArray());
+            Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
+
+            Debug.Log(gestureResult.GestureClass + " " + gestureResult.Score);
+
+            if (gestureResult.GestureClass.Equals("square"))
+            {
+                _assembledObject.Attack(FaceType.Square);
+            }
+            else if (gestureResult.GestureClass.Equals("circle"))
+            {
+                var clone = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                clone.transform.position = Vector3.zero;
+            }
+        }
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyUp(KeyCode.R))
+
+
+            if (pointerDown)
             {
-                recognized = true;
-
-                Gesture candidate = new Gesture(points.ToArray());
-                Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
-
-                Debug.Log( gestureResult.GestureClass + " " + gestureResult.Score);
-
-                if(gestureResult.GestureClass.Equals("square"))
-                {
-                   _assembledObject.Attack(FaceType.Square);
-                } 
-                else if (gestureResult.GestureClass.Equals("circle"))
-                {
-                    var clone = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    clone.transform.position = Vector3.zero;
-                }
-            }
-
-            
-            if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
-            {
-                if (Input.touchCount > 0)
+                if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
                 {
                     virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
                 }
-            }
-            else
-            {
-                if (Input.GetMouseButton(0))
+                else
                 {
                     virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
                 }
-            }
 
-            if (drawArea.Contains(virtualKeyPosition))
-            {
-
-                if (Input.GetMouseButtonDown(0))
+                if (drawArea.Contains(virtualKeyPosition))
                 {
 
-                    if (recognized)
+                    if (Input.GetMouseButton(0))
                     {
+                        points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
 
-                        recognized = false;
-                        strokeId = -1;
-
-                        points.Clear();
-
-                        foreach (LineRenderer lineRenderer in gestureLinesRenderer)
-                        {
-
-                            lineRenderer.SetVertexCount(0);
-                            Destroy(lineRenderer.gameObject);
-                        }
-
-                        gestureLinesRenderer.Clear();
+                        currentGestureLineRenderer.positionCount = ++vertexCount;
+                        currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
                     }
-
-                    ++strokeId;
-
-                    Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
-                    currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
-
-                    gestureLinesRenderer.Add(currentGestureLineRenderer);
-
-                    vertexCount = 0;
-                }
-
-                if (Input.GetMouseButton(0))
-                {
-                    points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
-
-                    currentGestureLineRenderer.SetVertexCount(++vertexCount);
-                    currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
                 }
             }
         }
