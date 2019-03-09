0416b1fb2a248b461ea5a0e79154d42c34364ea0
RustamA <rustam.alashrafov@gmail.com>
2019-03-09 20:13:52 +0100

    Made the scene setup. Assembled object now flies into the planet.

--- Assets/Scripts/AssembledObject.cs
+++ Assets/Scripts/AssembledObject.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 namespace AstroNet.GameElements
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class AssembledObject : MonoBehaviour
     {
+        [SerializeField] Vector2 _initialTorqueRange;
+        [SerializeField] Vector2 _initialForceRange;
+        [SerializeField] Transform _target;
         [SerializeField] List<Transform> _faces;
+
+        private Rigidbody _rigidBody;
         // Start is called before the first frame update
         public void Attack(FaceType type)
         {
@@ -20,6 +26,34 @@ namespace AstroNet.GameElements
                 }
 
             }
+        }
+
+        protected void Start()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+            ApplyInitialTorque();
+            ApplyInitialForce();
+
+        }
+
+        [ContextMenu("ApplyTorque")]
+        private void ApplyInitialTorque()
+        {
+            _rigidBody.angularVelocity = Vector3.zero;
+            var tx = Random.Range(_initialTorqueRange.x, _initialTorqueRange.y);
+            var ty = Random.Range(_initialTorqueRange.x, _initialTorqueRange.y);
+            var tz = Random.Range(_initialTorqueRange.x, _initialTorqueRange.y);
+            var randomTorque = new Vector3(tx, ty, tz) * _rigidBody.mass;
+
+            _rigidBody.AddRelativeTorque(randomTorque, ForceMode.VelocityChange);
+        }
+        [ContextMenu("ApplyForce")]
+        private void ApplyInitialForce()
+        {
+            _rigidBody.velocity = Vector3.zero;
+            var direction = _target.position - transform.position;
+            var force = direction * Random.Range(_initialForceRange.x, _initialForceRange.y);
+            _rigidBody.AddForce(force, ForceMode.VelocityChange);
         }
     }
 }
